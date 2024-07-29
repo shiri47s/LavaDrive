680bbe0f1100f85c905b5a3afa03acb3b462b9c0
shima <sh1m@outlook.jp>
2024-07-29 17:08:18 +0900

    Creative mode is now supported.

--- common/src/main/java/com/shiri47s/mod/lavadrive/LavaDrive.java
+++ common/src/main/java/com/shiri47s/mod/lavadrive/LavaDrive.java
@@ -3,8 +3,10 @@ package com.shiri47s.mod.lavadrive;
 import com.shiri47s.mod.lavadrive.items.*;
 import com.shiri47s.mod.lavadrive.materials.LavaArmorMaterials;
 import dev.architectury.event.events.client.ClientTickEvent;
+import dev.architectury.registry.CreativeTabRegistry;
 import dev.architectury.registry.level.biome.BiomeModifications;
 import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.DeferredSupplier;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
@@ -12,14 +14,13 @@ import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.enums.Instrument;
 import net.minecraft.entity.damage.DamageType;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.BiomeTags;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.PlacedFeature;
 
@@ -27,6 +28,9 @@ import net.minecraft.world.gen.feature.PlacedFeature;
 public class LavaDrive
 {
 	public static final String MOD_ID = "lavadrive";
+
+	private static final DeferredRegister<ItemGroup> TAB_REGISTER = DeferredRegister.create(MOD_ID, RegistryKeys.ITEM_GROUP);
+	public static DeferredSupplier<ItemGroup> TAB_SUPPLIER;
 
 	public static final RegistryKey<PlacedFeature> RED_DIAMOND_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(MOD_ID, Ids.RedDiamondPlacedKey));
 	public static final RegistryKey<PlacedFeature> RED_DIAMOND_HIGHER_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(MOD_ID, Ids.RedDiamondHigherPlacedKey));
@@ -124,6 +128,11 @@ public class LavaDrive
 
 	@SuppressWarnings("UnstableApiUsage")
 	public static void init(IModPlatform platform) {
+		TAB_SUPPLIER = TAB_REGISTER.register(MOD_ID, () -> CreativeTabRegistry.create(
+				Text.translatable("item.lavadrive"),
+				() -> new ItemStack(Items.LAVA_BUCKET)));
+		TAB_REGISTER.register();
+
 		LavaDriveBlocks.register();
 		LavaDriveItems.register();
 
